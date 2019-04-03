db991671619159489c087f526c63d3aed466b9b3
mirokshi <rojasdiazmirokshi@gmail.com>
2019-04-03 11:38:51 +0200

    Sync

--- app/helpers.php
+++ app/helpers.php
@@ -726,3 +726,49 @@ if (!function_exists("migrate_activitats")){
         ]);
     }
 }
+
+if (!function_exists('create_mysql_database')){
+    function create_mysql_database($name){
+        //PDO
+        // MySQL : CREATE DATABASE IF NOT EXISTS $name
+        $statement = "CREATE DATABASE IF NOT EXISTS $name";
+        DB::connection('mysqlroot')->getPdo()->exec($statement);
+    }
+}
+if (!function_exists('drop_mysql_database')){
+    function drop_mysql_database($name){
+        //PDO
+        // MySQL : DROP DATABASE IF NOT EXISTS $name
+        $statement = "DROP DATABASE IF EXISTS $name";
+        DB::connection('mysqlroot')->getPdo()->exec($statement);
+    }
+}
+if (!function_exists('create_mysql_user')){
+    function create_mysql_user($name, $password = null, $host='localhost'){
+        //PDO
+        if (!$password) $password =str_random();
+        $statement = "CREATE USER IF NOT EXISTS {$name}@{$host}";
+        DB::connection('mysqlroot')->getPdo()->exec($statement);
+        $statement = "ALTER USER '{$name}'@'{$host}' IDENTIFIED BY '{$password}'";
+        DB::connection('mysqlroot')->getPdo()->exec($statement);
+        return $password;
+    }
+}
+if (!function_exists('grant_mysql_privileges')) {
+    function grant_mysql_privileges($user, $database, $host = 'localhost')
+    {
+        //PDO
+        $statement = "GRANT ALL PRIVILEGES ON {$database}.* TO '{$user}'@'{$host}' WITH GRANT OPTION";
+        DB::connection('mysqlroot')->getPdo()->exec($statement);
+        $statement = "FLUSH PRIVILEGES";
+        DB::connection('mysqlroot')->getPdo()->exec($statement);
+    }
+}
+if (!function_exists('create_database')) {
+    function create_database()
+    {
+        create_mysql_database(env('DB_DATABASE'));
+        create_mysql_user(env('DB_USERNAME'),env('DB_PASSWORD'));
+        grant_mysql_privileges(env('DB_USERNAME'),env('DB_DATABASE'));
+    }
+}
