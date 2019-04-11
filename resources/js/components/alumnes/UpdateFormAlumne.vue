e2c22356705bb803b261946fa24192cf02334258
mirokshi <rojasdiazmirokshi@gmail.com>
2019-04-11 19:49:52 +0200

    Sync

--- resources/js/components/alumnes/UpdateFormAlumne.vue
+++ resources/js/components/alumnes/UpdateFormAlumne.vue
@@ -1,7 +1,7 @@
 <template>
     <v-form>
         <v-container>
-            <div class="headline font-weight-light grey--text">DADES DEL ALUMNE</div>
+            <div class="headline font-weight-light grey--text">DADES DEL AAALUMNE</div>
             <v-layout row wrap>
                 <v-flex xs12 sm6 >
                     <v-text-field
